2b28941c1da61f6e0f356716660e4f740d33786e
kai0310 <kn0310@outlook.jp>
2021-03-31 17:27:26 +0900

    wip

---
+++ README.md
@@ -0,0 +1,9 @@
+# OpinionBox wiki
+
+
+```Bash
+$ yarn install
+$ yarn dev
+```
+
+Get access [http://localhost:8080]( http://localhost:8080) 
--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -63,6 +63,7 @@ module.exports = {
           collapsable: false,
           children: [
             '',
+            'post-opinion',
             'account',
           ]
         }
